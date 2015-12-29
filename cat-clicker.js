4d94e5eb80bc4098f5285f7e7eca35506bf95c97
Tim JK Strickland <timjkstrickland@gmail.com>
2015-12-28 22:27:56 -0800

    Create
    Hello world creation of cat clicker for Udacity

---
+++ cat-clicker.js
@@ -0,0 +1,4 @@
+var $cat1 = document.getElementById('#cat1');
+var $cat2 = document.getElementById('#cat2');
+$cat1.style.background("url:'./img/cat1.jpg'");
+$cat2.style.background("url:'./img/cat2.jpg'");
