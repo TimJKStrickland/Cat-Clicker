4fdd7f538ce551a5cc9583f02234d3b82f3fd9d2
Tim JK Strickland <timjkstrickland@gmail.com>
2015-12-29 14:29:15 -0800

    Update

--- cat-clicker.js
+++ cat-clicker.js
@@ -2,3 +2,12 @@ var $cat1 = document.getElementById('#cat1');
 var $cat2 = document.getElementById('#cat2');
 $cat1.style.background("url:'./img/cat1.jpg'");
 $cat2.style.background("url:'./img/cat2.jpg'");
+var cat1count = 0;
+
+$(cat1).click(function(){
+  $cat1count++;
+});
+$(cat2).click(function(){
+  $cat2count++;
+});
+
