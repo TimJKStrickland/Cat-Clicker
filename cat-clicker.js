ae896ea86ff33dcf39a97daee331f11b0b529ee4
Tim JK Strickland <timjkstrickland@gmail.com>
2015-12-30 08:13:36 -0800

    Update
    Fix cat-clicker

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,13 +1,10 @@
 var $cat1 = document.getElementById('#cat1');
 var $cat2 = document.getElementById('#cat2');
-$cat1.style.background("url:'./img/cat1.jpg'");
-$cat2.style.background("url:'./img/cat2.jpg'");
 var cat1count = 0;
-
-$(cat1).click(function(){
+var cat2count = 0;
+$cat1.click(function(){
   $cat1count++;
 });
-$(cat2).click(function(){
+$cat2.click(function(){
   $cat2count++;
 });
-
